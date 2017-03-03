6fc65d0ba9f943e3effb67d987b681985c82ec66
Jitae Kim <originerd@gmail.com>
2017-03-04 00:23:40 +0900

    Fix range issue on scala implementation

--- src/Lotto.scala
+++ src/Lotto.scala
@@ -1,5 +1,5 @@
 import scala.util.Random;
 
 object Lotto extends App {
-  println(Random.shuffle(1 to 46).take(6).sorted.mkString(", "))
+  println(Random.shuffle(1 to 45).take(6).sorted.mkString(", "))
 }
