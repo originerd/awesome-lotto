f2b6fc81b88e0c14379d07c59a7ac11f736d12bd
Developer <developer@softdevstory.net>
2017-03-20 21:43:55 +0900

    Update lotto.swift

--- src/lotto.swift
+++ src/lotto.swift
@@ -1,17 +1,22 @@
-import Foundation
-
 var numbers: [Int] = []
 
 for number in 1...45 {
     numbers.append(number)
 }
 
+#if os(Linux)
+import Glibc
+numbers.sort{ (_, _) in random() < random() }
+#else
+import Foundation
 numbers.sort{ (_, _) in arc4random() < arc4random() }
+#endif
+
+var lottoNumbers: [Int] = []
+for _ in 1...6 {
+    lottoNumbers.append(numbers.popLast()!)
+}
+lottoNumbers.sort()
 
 print("Lotto numbers: ", terminator: "");
-print("\(numbers.popLast()!)", terminator: " ");
-print("\(numbers.popLast()!)", terminator: " ");
-print("\(numbers.popLast()!)", terminator: " ");
-print("\(numbers.popLast()!)", terminator: " ");
-print("\(numbers.popLast()!)", terminator: " ");
-print("\(numbers.popLast()!)");
+print(lottoNumbers)
