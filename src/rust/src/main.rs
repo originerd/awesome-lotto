7fbcbbb2468fcd86430224684d58db1875e2caef
taromati <2logta@gmail.com>
2017-03-20 18:08:46 +0900

    Add Rust version

---
+++ src/rust/src/main.rs
@@ -0,0 +1,32 @@
+extern crate rand;
+
+use std::vec::Vec;
+use rand::distributions::{IndependentSample, Range};
+
+fn main() {
+    let mut lotto_numbers = Vec::new();
+    for n in 0..46 {
+        lotto_numbers.push(n+1);
+    }
+
+    let mut rng = rand::thread_rng();
+    let mut result_numbers = Vec::new();
+    for _ in 0..6 {
+       	let between = Range::new(0, lotto_numbers.len()-1);
+
+        let index = between.ind_sample(&mut rng);
+        result_numbers.push(lotto_numbers[index]);
+        lotto_numbers.remove(index);
+    }
+
+    result_numbers.sort();
+
+    let result_string = result_numbers.iter()
+        .fold(String::new(), |a, b| {
+                if a.len() > 0 {
+                    return a + ", " + &(b.to_string());
+                }
+                b.to_string()
+        });
+    println!("{}", result_string);
+}
