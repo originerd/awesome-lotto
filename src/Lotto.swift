07798702e3863894b25ae1c7550187a38701edeb
Jitae Kim <originerd@gmail.com>
2017-03-20 23:01:24 +0900

    Refactor swift implementation for more clarity

--- src/Lotto.swift
+++ src/Lotto.swift
@@ -1,12 +1,9 @@
-func a(r: () -> UInt32) {
-    print(Array(1...45).sorted { (_, _) in r() % 2 == 0 }[0..<6].sorted().map { String($0) }.joined(separator: ", "));
-}
-
 #if os(Linux)
-import Glibc
-a(r: random)
-
+    import Glibc
+    let rand = random;
 #else
-import Foundation
-a(r: arc4random)
+    import Foundation
+    let rand = arc4random;
 #endif
+
+print(Array(1...45).sorted { (_, _) in rand() % 2 == 0 }[0..<6].sorted().map { String($0) }.joined(separator: ", "));
