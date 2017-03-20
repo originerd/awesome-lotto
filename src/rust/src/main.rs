b41d09afe6086492a79350a8b8ac2fc351facd3f
taromati <2logta@gmail.com>
2017-03-20 18:20:19 +0900

    using sample method

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,27 +1,14 @@
 extern crate rand;
 
-use std::vec::Vec;
-use rand::distributions::{IndependentSample, Range};
+use rand::{thread_rng, sample};
 
 fn main() {
-    let mut lotto_numbers = Vec::new();
-    for n in 0..45 {
-        lotto_numbers.push(n+1);
-    }
+    let mut rng = thread_rng();
+    let mut lotto_numbers = sample(&mut rng, 1..45, 6);;
 
-    let mut rng = rand::thread_rng();
-    let mut result_numbers = Vec::new();
-    for _ in 0..6 {
-       	let between = Range::new(0, lotto_numbers.len()-1);
-
-        let index = between.ind_sample(&mut rng);
-        result_numbers.push(lotto_numbers[index]);
-        lotto_numbers.remove(index);
-    }
-
-    result_numbers.sort();
-
-    let result_string = result_numbers.iter()
+    lotto_numbers.sort();
+    
+    let result_string = lotto_numbers.iter()
         .fold(String::new(), |a, b| {
                 if a.len() > 0 {
                     return a + ", " + &(b.to_string());
