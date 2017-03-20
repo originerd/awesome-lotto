dcca0f702ef73f64cb075a7ff35fa8badf9e07dd
Developer <developer@softdevstory.net>
2017-03-20 22:54:00 +0900

    Update Lotto.swift

--- src/Lotto.swift
+++ src/Lotto.swift
@@ -1,3 +1,12 @@
-import Foundation
+func a(r: () -> UInt32) {
+    print(Array(1...45).sorted { (_, _) in r() % 2 == 0 }[0..<6].sorted().map { String($0) }.joined(separator: ", "));
+}
 
-print(Array(1...45).sorted { (_, _) in arc4random() % 2 == 0 }[0..<6].sorted().map { String($0) }.joined(separator: ", "));
+#if os(Linux)
+import Glibc
+a(r: random)
+
+#else
+import Foundation
+a(r: arc4random)
+#endif
